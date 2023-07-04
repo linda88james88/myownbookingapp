53c2306799b42c593a3b98b20e8324b660da422a
linda88james88 <linda88james88@gmail.com>
2023-07-04 20:55:51 +0100

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css';
+
+export default function Main() {
+  return (
+    <main id='main'> 
+
+      <div className='booking'>
+        {/* <img src='https://mohammedthoufiq.simplybook.me/v2/themes/assets/img/categories/appearance.jpg' alt=''></img> */}
+      <h1 >Muhammathrafiq</h1>
+      <button>Book Now <i className="fa fa-check fa-check"></i></button>
+      </div>
+      
+    </main>
+  )
+}
